59cf8e20b10edfe1b1683c7bfb07b09ac6a2cc50
Luis Fernando Rodrigues Hayashi <luisfrhayashi@hotmail.com>
2020-05-06 19:49:15 -0300

    Novo mudança

---
+++ BotLeituraExcell/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BotLeituraExcell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
